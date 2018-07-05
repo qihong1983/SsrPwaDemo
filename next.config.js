9a4d315a551a6a23e5720ab057c64279efdc50de
qihong1 <qihong1@lenovo.com>
2018-07-06 02:19:54 +0800

    aaaa

--- next.config.js
+++ next.config.js
@@ -18,6 +18,29 @@ var config = {
 
 
 module.exports = withOffline({
+  webpack: (config, {
+        dev
+    }) => {
+        config.module.rules.push({
+            test: /\.css$/,
+            loader: 'emit-file-loader',
+            options: {
+                name: 'dist/[path][name].[ext]',
+            },
+        }, {
+            test: /\.css$/,
+            // Simplest way (non-minified).. 
+            loader: `babel-loader!next-style-loader`,
+            // Use `css-loader` to minify and enable source maps 
+            // NOTE: The `url` option from the css loader must be disabled; images, fonts, etc should go into /static 
+            loader: `babel-loader!next-style-loader!css-loader?sourceMap&minimize=true&url=false`,
+            // Same as above but with CSS modules 
+            loader: `babel-loader!next-style-loader!css-loader?sourceMap&minimize=true&url=false&modules`,
+
+        });
+
+        return config;
+    },
  exportPathMap: async function (defaultPathMap) {
     return {
       '/': { page: '/' },
