226df4d5fec91460c6daff974a36a11fe8d16713
qihong1 <qihong1@lenovo.com>
2018-07-06 02:31:55 +0800

    ddf

--- pages/index.js
+++ pages/index.js
@@ -92,15 +92,5 @@ class Index extends React.Component {
 	}
 }
 
-import Link from 'next/link'
-//import Link from 'next/prefetch'
-const Index = () => (
-  <div>
-    <Link href="/about?id=111">
-      <a>About Page</a>
-    </Link>
-    <p>Hello ssr+pwa</p>
-  </div>
-)
 
 export default Index
